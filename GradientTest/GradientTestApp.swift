f8a8400da3abc748e002702f421df146819cdcde
Vlad <xalif80@gmail.com>
2024-11-05 13:18:48 +0100

    Initial Commit

---
+++ GradientTest/GradientTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientTestApp.swift
+//  GradientTest
+//
+//  Created by Vladimir Martemianov on 5.11.24..
+//
+
+import SwiftUI
+
+@main
+struct GradientTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
